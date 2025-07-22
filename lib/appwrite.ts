2c3190c200f9366fb0a21669acbec8880b4ab578
ParasChhetri <paraschhetri477@gmail.com>
2025-07-23 03:26:33 +0530

    sign up and sign in screen Ui made

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
     endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     platform : "com.paras.fastfood",
-    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId : process.env.DATABASE_ID,
+    userCollectionID : process.env.USER_COLLECTION_ID
 }
