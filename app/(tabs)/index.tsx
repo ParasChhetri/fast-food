0e7b8056ecfd3f577b1d65e7e7f6882a3974b4c5
ParasChhetri <paraschhetri477@gmail.com>
2025-07-23 00:09:06 +0530

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,58 @@
+import CartButton from "@/components/CartButton";
+import { images, offers } from "@/constants";
+import { FlatList, Image, Pressable, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList 
+        data={offers}
+        renderItem={({item, index}) => {
+          const isEven = index % 2 === 0;
+          return (
+            <View>
+              <Pressable 
+                className={`offer-card h-48 ${isEven ? "flex-row-reverse" : "flex-row"}`} 
+                style={{backgroundColor : item.color}}
+                android_ripple={{color : '#ffffff22'}}
+              >
+                {({pressed}) => (
+                    <>
+                      <View className="h-full w-1/2">
+                        <Image source={item.image} className="size-full" resizeMode="cover"/>
+                      </View>
+                      <View className={`offer-card__info ${isEven ? 'pl-2' : 'pr-2'}`}>
+                        <Text className="h1-bold text-white leading-tight">{item.title}</Text>
+                        <Image 
+                          source={images.arrowRight} className="size-10" resizeMode="contain" 
+                          tintColor="#ffffff"
+                        />
+                      </View>
+                    </>
+                )}
+              </Pressable>
+            </View>
+          )
+        }}
+        contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Punjab</Text>
+                <Image 
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+            <CartButton />
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+}
---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    platform : "com.paras.fastfood",
+    projectId : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID
+}
